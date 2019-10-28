95add9f4011d1102c90a4670b8873bc0a3ed650d
tufail97 <tufail.rahman@adyli.com>
2019-10-28 22:02:26 +0000

    creted basic loader animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const LoaderWrapper = styled.div`
   width: 100%;
@@ -19,13 +19,24 @@ const LoaderWrapper = styled.div`
     animation-iteration-count: infinite;
     animation-timing-function: ease;
   }
+  div:nth-child(1) {
+    animation-delay: 0.5s;
+  }
+
+  div:nth-child(2) {
+    animation-delay: 1s;
+  }
+
+  div:nth-child(3) {
+    animation-delay: 1.5s;
+  }
 
   @keyframes loader-animation {
     0% {
       transform: scaleY(1.2) scaleX(0.9);
     }
     50% {
-      transform: scaleY(1.2) scaleX(0.9);
+      transform: scaleY(1) scaleX(0.9);
     }
     100% {
       transform: scaleY(1.2) scaleX(0.9);
