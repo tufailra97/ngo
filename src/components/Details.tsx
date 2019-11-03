3668bf6dd451d6d09a2b8a1918e5add931a9a6c4
tufail97 <tufail.rahman@adyli.com>
2019-11-03 12:18:12 +0000

    init detail component

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,20 +1,129 @@
 import React from 'react';
-import { IMovie, IMovieDetails, ICredits } from 'interfaces';
+import { IMovieDetails } from 'interfaces';
+import { ICast } from 'interfaces/MovieProps';
+import { Headline, Subline, Paragraph } from 'elements/Typography';
+import styled from 'styled-components';
+import Genres from './details/Genres';
+
+const DetailsWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+
+  .image-container {
+    flex-shrink: 0;
+    width: 20vw;
+    height: auto;
+    margin-right: 1rem;
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .details-container {
+    margin-left: 1rem;
+    width: 40vw;
+
+    .headline {
+      margin-bottom: 1rem;
+      h1,
+      h2 {
+        max-width: 40rem;
+      }
+
+      h1 {
+        margin-bottom: 1rem;
+      }
+    }
+    .paragraph {
+      font-size: 1.3rem;
+      line-height: 1.4;
+    }
+    /* cast images */
+    .cast-img {
+      width: 4rem;
+      height: 4rem;
+      display: flex;
+
+      img {
+        border-radius: 50%;
+        width: 4rem;
+        height: 4rem;
+      }
+    }
+  }
+`;
 
 interface IDetails {
   movie: IMovieDetails;
-  cast: ICredits;
+  cast: Array<ICast>;
+  callback: Function;
 }
 
-const Details: React.FC<IDetails & ICredits> = ({ movie, cast }) => {
+const Details: React.FC<IDetails> = ({ movie, cast, callback }) => {
+  const handleCast = ():
+    | Array<React.ReactElement>
+    | React.ReactElement
+    | null => {
+    let casts = [];
+    let crews = cast;
+    if (Array.isArray(crews)) {
+      if (crews.length > 20) {
+        crews = crews.slice(0, 10);
+      }
+      casts = crews.map(cast => {
+        return (
+          <div
+            className='cast-img'
+            key={cast.id}
+            onClick={() => {
+              callback(cast.id);
+            }}
+          >
+            <img src={`https://image.tmdb.org/t/p/w780/${cast.profile_path}`} />
+          </div>
+        );
+      });
+      return casts;
+    }
+    return null;
+  };
+
   return (
-    <div className='details-wrapper'>
+    <DetailsWrapper>
       <div className='image-container'>
         <img src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`} />
       </div>
       <div className='details-container'>
         {/* title */}
-        <h1>{movie.original_title}</h1>
+        <div className='headline'>
+          <Headline
+            className='desc-item'
+            style={{
+              fontWeight: 400,
+              maxWidth: '40rem',
+              fontSize: '3.5rem',
+              textTransform: 'uppercase'
+            }}
+          >
+            {movie.original_title}
+          </Headline>
+          {movie.tagline && (
+            <Subline
+              className='desc-item'
+              style={{
+                fontWeight: 600,
+                maxWidth: '40rem',
+                fontSize: '1.5rem',
+                letterSpacing: 1,
+                textTransform: 'uppercase'
+              }}
+            >
+              {movie.tagline}
+            </Subline>
+          )}
+        </div>
         <div className='info'>
           {/* rating */}
           <div className='rating'>
@@ -23,34 +132,56 @@ const Details: React.FC<IDetails & ICredits> = ({ movie, cast }) => {
           </div>
           {/* release date */}
           <div>
-            <h3>Release Date</h3>
-            <p>{movie.release_date}</p>
+            <Subline style={{ fontSize: '1.3rem', textTransform: 'uppercase' }}>
+              Release Date
+            </Subline>
+            <Paragraph className='paragraph desc-item'>
+              {movie.release_date}
+            </Paragraph>
           </div>
-          <div>
-            <h3>Runtime</h3>
-            <p>{movie.runtime}min</p>
+          <div className='run-time'>
+            <Subline style={{ fontSize: '1.3rem', textTransform: 'uppercase' }}>
+              Runtime
+            </Subline>
+            <Paragraph className='paragraph desc-item'>
+              {movie.runtime}min
+            </Paragraph>
+          </div>
+          <div className='genres'>
+            <Genres genres={movie.genre_ids!} />
           </div>
           {/* description */}
-          <div>
-            <h3>Description</h3>
+          <div className='description'>
+            <Subline style={{ fontSize: '1.3rem', textTransform: 'uppercase' }}>
+              Description
+            </Subline>
             <div className='description'>
-              <p>{movie.overview}</p>
+              <Paragraph className='paragraph desc-item'>
+                {movie.overview}
+              </Paragraph>
             </div>
           </div>
           {/* production company */}
           <div className='production-companies'>
-            <h3>Production Companies</h3>
-            <div>
+            <Subline style={{ fontSize: '1.3rem', textTransform: 'uppercase' }}>
+              Production Companies
+            </Subline>
+            <Paragraph className='paragraph desc-item'>
               {movie.production_companies.map(company => {
                 return <span key={company.id}>{company.name}</span>;
               })}
-            </div>
+            </Paragraph>
           </div>
           {/* cast */}
-          {/* <div className='cast-img'>{handleCast()}</div> */}
+          <div className='casts'>
+            <Subline style={{ fontSize: '1.3rem', textTransform: 'uppercase' }}>
+              Cast
+            </Subline>
+            <div className='cast-img'>{handleCast()}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </DetailsWrapper>
   );
 };
 
---
+++ src/components/details/Genres.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+interface IGenres {
+  genres: Array<{ id: number; name: string }>;
+}
+
+const genres = [
+  {
+    id: 28,
+    name: 'Action'
+  },
+  {
+    id: 12,
+    name: 'Adventure'
+  },
+  {
+    id: 16,
+    name: 'Animation'
+  },
+  {
+    id: 35,
+    name: 'Comedy'
+  },
+  {
+    id: 80,
+    name: 'Crime'
+  },
+  {
+    id: 99,
+    name: 'Documentary'
+  },
+  {
+    id: 18,
+    name: 'Drama'
+  },
+  {
+    id: 10751,
+    name: 'Family'
+  },
+  {
+    id: 14,
+    name: 'Fantasy'
+  },
+  {
+    id: 36,
+    name: 'History'
+  },
+  {
+    id: 27,
+    name: 'Horror'
+  },
+  {
+    id: 10402,
+    name: 'Music'
+  },
+  {
+    id: 9648,
+    name: 'Mystery'
+  },
+  {
+    id: 10749,
+    name: 'Romance'
+  },
+  {
+    id: 878,
+    name: 'Science Fiction'
+  },
+  {
+    id: 10770,
+    name: 'TV Movie'
+  },
+  {
+    id: 53,
+    name: 'Thriller'
+  },
+  {
+    id: 10752,
+    name: 'War'
+  },
+  {
+    id: 37,
+    name: 'Western'
+  }
+];
+
+const Genres: React.FC<IGenres> = ({ genres }) => {
+  console.log('genres ----> ', genres);
+
+  return <div>Genres</div>;
+};
+
+export default Genres;
