eb51e51f08af3a2e69834d8ce313a01d69cc3b56
tufail97 <tufail.rahman@adyli.com>
2019-11-05 22:37:21 +0000

    produced search icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,12 @@
 import Home from './Home';
 import Movie from './Movie';
 import Series from './Series';
+import Search from './Search';
 import FullStar from './FullStar';
 import HalfStar from './HalfStar';
 import EmptyStar from './EmptyStar';
 import ArrowFull from './ArrowFull';
 import LeftArrow from './LeftArrow';
 import RightArrow from './RightArrow';
-export { Home, Movie, Series, FullStar, EmptyStar, HalfStar, ArrowFull, LeftArrow, RightArrow };
+
+export { Home, Movie, Series, FullStar, EmptyStar, HalfStar, ArrowFull, LeftArrow, RightArrow, Search };
