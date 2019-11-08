a5b0523d85fbf1463bac03484d1c185c6a21a155
tufail97 <tufail.rahman@adyli.com>
2019-11-08 09:42:42 +0000

    produced action search

--- src/actions/_search.ts
+++ src/actions/_search.ts
@@ -1,21 +1,20 @@
 import { SEARCH_REQEUSTED, SEARCH_FAILED, SEARCH_RESULTS } from 'types'
 import axios from 'axios';
 import { Dispatch } from 'redux'
+import { ISearch } from 'interfaces';
 
 export const search = (keywords: string) => async (dispatch: Dispatch) => {
   dispatch({ type: SEARCH_REQEUSTED });
   const url = `${process.env.REACT_APP_TMDB_END_POINT}/search/multi?api_key=${process.env.REACT_APP_TMDB_API}&query=${keywords}&language=en-US`;
 
   const request = await axios.get(url);
-  const data: any = request.data;
+  const data: ISearch = request.data;
   const status = request.status;
-
-  console.log('res', data);
 
   if (status === 200) {
     return dispatch({
       type: SEARCH_RESULTS,
-      results: data.results,
+      results: data
     });
   }
   return dispatch({
