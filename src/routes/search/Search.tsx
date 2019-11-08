07a2dabda8fbcfb70da399d7d570dffb49c4fb8b
tufail97 <tufail.rahman@adyli.com>
2019-11-08 22:14:02 +0000

    search logic completed

--- src/routes/search/Search.tsx
+++ src/routes/search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import {
   ISearchInitialState,
@@ -7,6 +7,9 @@ import {
   ISearchPerson
 } from 'interfaces';
 import styled from 'styled-components';
+import { Card } from 'components';
+import { Subline } from 'elements/Typography';
+import { useHistory } from 'react-router-dom';
 
 const SearchWrapper = styled.div`
   margin-top: 2rem;
@@ -14,19 +17,104 @@ const SearchWrapper = styled.div`
 `;
 
 const Search: React.FC = () => {
+  const history = useHistory();
   const search: ISearchInitialState = useSelector((state: any) => state.search);
   let movies: ISearchMovie | Array<ISearchMovie> = [],
     series: ISearchSerie | Array<ISearchSerie> = [],
     persons: ISearchPerson | Array<ISearchPerson> = [];
+  let personsContainer: Array<ReactElement> = [];
+  let seriesContainer: Array<ReactElement> = [];
+  let moviesContainer: Array<ReactElement> = [];
+
+  const handlePersonDetails = (id: number) => {
+    history.replace({
+      pathname: `/people/${id}`
+    });
+  };
+  const handleMovieDetails = (id: number) => {
+    history.push({
+      pathname: `/movies/details/${id}`
+    });
+  };
+  const handleSerieDetails = (id: number) => {
+    history.replace({
+      pathname: `/series/details/${id}`
+    });
+  };
+
   if (search) {
     movies = search.results.movies!;
     series = search.results.series!;
     persons = search.results.persons!;
   }
 
-  console.log('movies', movies);
+  if (Array.isArray(persons) && persons.length > 0) {
+    personsContainer = persons.map(person => {
+      return (
+        <Card
+          id={person.id}
+          key={person.id}
+          title={person.name!}
+          imageURL={person.profile_path!}
+          callback={handlePersonDetails}
+        />
+      );
+    });
+  }
 
-  return <SearchWrapper>Search</SearchWrapper>;
+  if (Array.isArray(series) && series.length > 0) {
+    seriesContainer = series.map(serie => {
+      return (
+        <Card
+          id={serie.id}
+          key={serie.id}
+          title={serie.name!}
+          imageURL={serie.poster_path!}
+          callback={handleSerieDetails}
+        />
+      );
+    });
+  }
+
+  if (Array.isArray(movies) && movies.length > 0) {
+    moviesContainer = movies.map(movie => {
+      return (
+        <Card
+          id={movie.id}
+          key={movie.id}
+          title={movie.title!}
+          imageURL={movie.poster_path!}
+          callback={handleMovieDetails}
+        />
+      );
+    });
+  }
+
+  return (
+    <SearchWrapper>
+      {/* movies */}
+      {moviesContainer.length > 0 && (
+        <div>
+          <Subline>Movies</Subline>
+          {moviesContainer}
+        </div>
+      )}
+      {/* series */}
+      {seriesContainer.length > 0 && (
+        <div>
+          <Subline>Series</Subline>
+          {seriesContainer}
+        </div>
+      )}
+      {/* persons */}
+      {personsContainer.length > 0 && (
+        <div>
+          <Subline>Persons</Subline>
+          {personsContainer}
+        </div>
+      )}
+    </SearchWrapper>
+  );
 };
 
 export default Search;
