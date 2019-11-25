3ba993d60d193afd36b0ed119b8713475a2969fa
tufail97 <tufail.rahman@adyli.com>
2019-11-25 14:25:27 +0000

    removed redux logger

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
