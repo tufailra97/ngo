06bea0620493615df4fa969b8c597aba484b0e44
tufail97 <tufail.rahman@adylic.com>
2019-12-01 22:11:41 +0000

    minor change

--- src/elements/Typography.tsx
+++ src/elements/Typography.tsx
@@ -2,6 +2,8 @@ import styled, { ThemeProps as StyleThemeProps } from 'styled-components';
 import { ThemeProps } from 'interfaces';
 
 export const Headline = styled.h1`
+  font-size: 3.5rem;
+  font-weight: 400;
   color: ${(props: StyleThemeProps<ThemeProps>) => props.theme.textColour};
 `;
 
