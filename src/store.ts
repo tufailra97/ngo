f6564e4187742164a90be67932b5020f11143a55
tufail97 <tufail.rahman@adylic.com>
2019-12-02 23:19:27 +0000

    minor change to redux  store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
+const loadState = (): Object => {
+  return JSON.parse(localStorage.getItem('store')!) || {};
+};
+
+const saveState = (state: Store): void => {
+  localStorage.setItem('store', JSON.stringify(state.getState()));
+};
+
 // initial state
-const initialState: any = {};
+const initialState: any = loadState();
 // set middlewares
 const middlewares = [thunk];
 // create store
@@ -13,5 +21,10 @@ const store = createStore(
   initialState,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
+
+// save store to localStorage
+store.subscribe(() => {
+  saveState(store);
+});
 
 export default store;
