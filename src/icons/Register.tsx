52556bb7bdd2bec48357eb5b14153337006d1388
tufail97 <tufail.rahman@adylic.com>
2019-12-03 22:43:46 +0000

    create new icons

---
+++ src/icons/Register.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { IconProps } from 'interfaces';
+
+export default ({ width, height, color }: IconProps): React.ReactElement => {
+  return (
+    <svg
+      version='1.1'
+      id='Layer_1'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      x='0px'
+      y='0px'
+      viewBox='0 0 512 512'
+      xmlSpace='preserve'
+      width={width}
+      height={height}
+      fill={color}
+    >
+      <path d='M304 256c52.805 0 96-43.201 96-96s-43.195-96-96-96-96 43.201-96 96 43.195 96 96 96zm0 48c-63.598 0-192 32.402-192 96v48h384v-48c0-63.598-128.402-96-192-96zM112 224v-64H80v64H16v32h64v64h32v-64h64v-32h-64z' />
+    </svg>
+  );
+};
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -10,7 +10,10 @@ import Close from './Close';
 import Avatar from './Avatar';
 import Series from './Series';
 import Search from './Search';
+import Login from './Login';
+import Logout from './Logout';
 import FullStar from './FullStar';
+import Register from './Register';
 import HalfStar from './HalfStar';
 import EmptyStar from './EmptyStar';
 import ArrowFull from './ArrowFull';
@@ -27,6 +30,9 @@ export {
   User,
   Email,
   Key,
+  Logout,
+  Login,
+  Register,
   FullStar,
   EmptyStar,
   HalfStar,
